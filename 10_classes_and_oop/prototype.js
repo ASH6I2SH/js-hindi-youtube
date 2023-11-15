e16c681592a60bafa7ad20f2a2033302d9714108
ASH6I2SH <ash6i2sh@gmail.com>
2023-11-15 21:00:09 +0530

    updated

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,14 @@
+// let myName='ashish     '
+// console.log(myName.truelength)
+
+let myHeros= ['thor', 'spiderman']
+
+let heroPower= {
+    thor: 'hammer',
+    spiderman: 'sling',
+    getSpiderPower: function(){
+
+        console.log(`spidy power is ${this.spiderman}`)
+    }
+
+}
